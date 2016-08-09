999f1daccf5164800fb639866b60117c0e44e575
Prince <princemwilliams@gmail.com>
2016-08-08 22:18:01 -0400

    Testing auth code tweaks from tutorial. Tutorial code causing error.

--- Chatter/App_Start/FilterConfig.cs
+++ Chatter/App_Start/FilterConfig.cs
@@ -8,6 +8,9 @@ namespace Chatter
         //all changes from tutorial worked to block/manage user access but the code below for some reason
         //still blocks user access with code on home and chat controllers even with this commented out...
 
+        //tried updating database and re-building still got error: "localhost refused to connect." 
+            //when using below tutorial code...
+
         //public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         //{
         //    filters.Add(new HandleErrorAttribute());
